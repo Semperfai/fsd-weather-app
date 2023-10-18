9bf9ed846a03d848f0899dfcc1a429a98a957a27
Semperfai <107950470+Semperfai@users.noreply.github.com>
2023-10-18 16:03:49 +0300

    Update forecast.ts

--- src/shared/api/weather/forecast.ts
+++ src/shared/api/weather/forecast.ts
@@ -27,21 +27,23 @@ export const getForecastByCity = (
 
 export const getUserForecast = async () => {
   try {
-    const response = await axios.get("https://httpbin.org/ip");
-    const userIP = response.data.origin.split(",")[0].trim();
-    const locationResponse = await axios.get(
-      `http://ip-api.com/json/${userIP}`,
-    );
-    const locationData = locationResponse.data;
-    const city = locationData.city;
-    const country = locationData.country;
+    
+    const response = await axios.get("https://ipinfo.io?token=d919b62edd0c89");
+
+    const locationData = response.data;
+    
+    
+    const { city, country } = locationData;
 
     const weatherData = await getForecastByCity({
       q: `${city},${country}`,
       units: "metric",
     });
+
     return weatherData;
+
   } catch (error) {
     console.error(error);
   }
+
 };
